abbe8220e6c390acc7b487f9f1df7a62530924ff
AlesVymazal <alda.vymazal@seznam.cz>
2024-02-08 10:21:48 +0100

    RadzenTest

---
+++ RadzenTest/Services/ColorService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RadzenTest.Services
+{
+    public class ColorService : ServiceCollection
+    {
+        public string GetColor(int number)
+        {
+            return number == 1 ? "red" : "green";
+        }
+    }
+}
